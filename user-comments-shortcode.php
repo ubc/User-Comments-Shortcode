edc2a039faeeec2138d77f18195d8223e32c2469
Richard Tape <richard.tape@ubc.ca>
2018-11-20 13:15:20 -0800

    Added fixes for PHP 7.2

--- user-comments-shortcode.php
+++ user-comments-shortcode.php
@@ -15,24 +15,24 @@ Usage: (many)
 *
 * You may need to use CSS for the output - as the HTML output area has a a <div> for
 * better control of styling the comment
-* 
-* 
+*
+*
 */
 
 add_shortcode ( 'user_comments', 'show_recent_comments_handler' );
 
 function show_recent_comments_handler ( $atts, $content = null )
 	{
-		extract( 
-			shortcode_atts( 
-				array( 
+		extract(
+			shortcode_atts(
+				array(
 					"count" => 150,
 				), $atts
 			)
 		);
 
 		$output = ''; // this holds the output until it's referenced later
-		
+
 		if ( is_user_logged_in() )
 		{
 			global $current_user;
@@ -45,9 +45,9 @@ function show_recent_comments_handler ( $atts, $content = null )
 			);
 
 			$comments = get_comments( $args );
-							
+
 				foreach ( $comments as $c )
-				{	
+				{
 
 					$output = $output . "<li>";
 					$output = $output . "</br>";
@@ -56,12 +56,12 @@ function show_recent_comments_handler ( $atts, $content = null )
 					$output = $output . "<b>";
 					$output = $output . $c->comment_author;
 					$output = $output . "</b>";
-					
+
 					$output = $output . " on ";
 
 					// comment date
 					$output = $output . "<i>";
-					$output = $output . get_comment_date (F-Y, $c);
+					$output = $output . get_comment_date ( 'F-Y', $c);
 					$output = $output . "</i>";
 
 					$output = $output . " wrote:";
@@ -72,22 +72,22 @@ function show_recent_comments_handler ( $atts, $content = null )
 					$output = $output . substr($c->comment_content ,0 ,350);
 					$output = $output . ' ..." ';
 
-					// link to comment				
-					$output = $output . '<a href="' . get_settings('siteurl') . '/?p=' . $c->comment_post_ID . '#comment-' . $c->comment_ID . '">';			
+					// link to comment
+					$output = $output . '<a href="' . get_settings('siteurl') . '/?p=' . $c->comment_post_ID . '#comment-' . $c->comment_ID . '">';
 					$output = $output . "[link to post]";
 					$output = $output . "</a>";
-					$output = $output . "</div>";					
+					$output = $output . "</div>";
 
 					// comment content style
-					$output = $output . "<style> 
+					$output = $output . "<style>
 					.comment_content { margin-top: 5px; }
-					</style>";	
+					</style>";
 
 					$output = $output . "</li>";
 					$output = $output . "</p>";
 
-				}			
-			
+				}
+
 		}
 
 		else
